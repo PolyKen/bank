2348a282cf8fbc060285dcb3e12653575869e5e8
PolyKen <kennyxz@live.cn>
2019-01-06 23:10:51 +0800

    fix query

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
     //render_table(["h1", "h2"], [["r1", "r2"], ["s1", "s2"]]);
 });
 
+let currency_type_dict = {"RMB": 1, "USD": 2, "EUR": 3, "JPY": 4, "GBP": 5, "HKD": 6};
+let deposit_type_dict = {"活期": 1, "定期三个月": 2, "定期六个月": 3, "定期一年": 4, "定期两年": 5, "定期三年": 6, "定期五年": 7};
 let empty_table = "<table id=\"table-display\" class=\"display\" style=\"width:100%\"><thead id=\"table-head\"></thead><tbody id=\"table-body\"></tbody><tfoot id=\"table-foot\"></tfoot></table>";
 
 function reloadDataTable() {
@@ -11,16 +13,18 @@ function reloadDataTable() {
 }
 
 function bind_event() {
-    $("#tab-1").on("click", function(){
+    $("#tab-1").on("click", function () {
         $("#tab-1 a").tab('show');
         $("#tab-1").addClass('active');
         $("#tab-2").removeClass('active');
     });
-    $("#tab-2").on("click", function(){
+
+    $("#tab-2").on("click", function () {
         $("#tab-2 a").tab('show');
         $("#tab-2").addClass('active');
         $("#tab-1").removeClass('active');
     });
+
     $("#btn-select-table").on("click", function () {
         let table_name = $("#table-name-to-select").val();
         $.get("/table/" + table_name, function (data) {
@@ -35,33 +39,34 @@ function bind_event() {
         })
     });
 
-    $("#btn-deposit").on("click", function(){
+    $("ul.dropdown-menu a").on("click", function () {
+        $(this).parents(".dropdown").children("button").text($(this).text() + " ");
+        $(this).parents(".dropdown").children("button").append("<span class=\"caret\"></span>");
+        $("#deposit_currency_type").text($(this).text());
+    });
+
+    $("#btn-deposit").on("click", function () {
         let user_id = $("#deposit_user_id").val();
         let account_id = $("#deposit_account_id").val();
         let quantity = $("#deposit_quantity").val();
-        let currency_type = $("#deposit_currency_type").val();
-        let deposit_type = $("#deposit_deposit_type").val();
-        let payload = {"user_id": user_id, "account_id": account_id, "quantity": quantity, "currency_type": currency_type, "deposit_type": deposit_type};
-        $.get("/deposit", payload, function(data){
+        let currency_type = $("#deposit_currency_type").text();
+        currency_type = currency_type_dict[currency_type];
+        let deposit_type = $("#deposit_deposit_type").text();
+        deposit_type = deposit_type_dict[deposit_type];
+        let payload = {
+            "user_id": user_id,
+            "account_id": account_id,
+            "quantity": quantity,
+            "currency_type": currency_type,
+            "deposit_type": deposit_type
+        };
+        $.get("/deposit", payload, function (data) {
             console.log("deposit");
             console.log(data);
             alert(data);
             $("#btn-deposit").parent().children("input").empty();
         })
     });
-
-    $("#btn-withdraw").on("click", function(){
-        let user_id = $("#withdraw_user_id").val();
-        let account_id = $("#withdraw_account_id").val();
-        let quantity = $("#withdraw_quantity").val();
-        let payload = {"user_id": user_id, "account_id": account_id, "quantity": quantity};
-        $.get("/withdraw", payload, function(data){
-            console.log("withdraw");
-            console.log(data);
-            alert(data);
-            $("#btn-withdraw").parent().children("input").empty();
-        })
-    })
 }
 
 function render_table(heads_list, rows_list) {
